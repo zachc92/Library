d32091d64a37b5b75b675869ffe4fb76cdf3c149
Zachary Cray <53413395+zachc92@users.noreply.github.com>
2026-06-07 17:04:17 -0400

    Refactor Library project to use classes and IIFEs

--- script.js
+++ script.js
@@ -1,65 +1,83 @@
-const bookTitle = document.querySelector("#title");
-const bookAuthor = document.querySelector("#author");
-const bookYearPublished = document.querySelector("#year_published");
-const bookRead = document.querySelector("#read_status");
-const addBookButton = document.querySelector("#add_book");
-const displayCase = document.querySelector(".display-case");
-
-const myLibrary = [];
-
-function Book(title, author, yearPublished, read) {
-    this.title = title;
-    this.author = author;
-    this.yearPublished = yearPublished;
-    this.id = crypto.randomUUID();
-    this.read = read;
-    this.readToggle = function(){
-        if(this.read === false){
-            this.read = true;
-        } else if(this.read === true){
-            this.read = false;
-        };
-    };
-};
-
-addBookButton.addEventListener("click", addBookToLibrary);
-
-function addBookToLibrary() {
-    const newBook = new Book(bookTitle.value, bookAuthor.value, bookYearPublished.value, bookRead.checked);
-    myLibrary.push(newBook);
-
-    const currentBook = newBook;
-
-    const child = displayCase.appendChild(document.createElement("div"));
-    child.id = currentBook.id;
-
-    const title = child.appendChild(document.createElement("p"));
-    title.textContent = `Title: ${currentBook.title}`;
-    const author = child.appendChild(document.createElement("p"));
-    author.textContent = ` Author: ${currentBook.author}`;
-    const yearPublished = child.appendChild(document.createElement("p"));
-    yearPublished.textContent = `Year Published: ${currentBook.yearPublished}`;
-
-    const removeButton = child.appendChild(document.createElement("button"));
-    removeButton.textContent = 'x';
-    removeButton.addEventListener("click", (e) => { 
-        child.remove();
-        for(let i=0;i<myLibrary.length;i++){
-            if(myLibrary[i].id == child.id){
-                myLibrary.splice(i, 1);
-                break;
-            }
-        }
-    });
-
-    const toggleButton = child.appendChild(document.createElement("button"));
-    if(currentBook.read === true){
-        toggleButton.textContent = "Read";
-    } else {
-        toggleButton.textContent = "Haven't read";
+class Book {
+    constructor(title, author, yearPublished, read) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+        this.read = read;
+        this.id = crypto.randomUUID();
     }
-    toggleButton.addEventListener("click", (e) => {
-        currentBook.readToggle();
-        currentBook.read === true ? toggleButton.textContent = "Read" : toggleButton.textContent = "Haven't Read";
-    });
+
+    readToggle() {
+        this.read = !this.read;
+    }
 };
+
+class Library {
+    constructor(library) {
+        this.library = [];
+    }
+}
+
+const eventListeners = (() => {
+    const bookTitle = document.querySelector("#title");
+    const bookAuthor = document.querySelector("#author");
+    const bookYearPublished = document.querySelector("#year_published");
+    const bookRead = document.querySelector("#read_status");
+    const addBookButton = document.querySelector("#add_book");
+    const displayCase = document.querySelector(".display-case");
+
+    addBookButton.addEventListener("click", () => {
+        controller.addBookToLibrary(bookTitle.value, bookAuthor.value, bookYearPublished.value, bookRead.checked);
+    });
+
+    const renderNewBook = (book) => {
+        const child = displayCase.appendChild(document.createElement("div"));
+        child.id = book.id;
+
+        const title = child.appendChild(document.createElement("p"));
+        title.textContent = `Title: ${book.title}`;
+        const author = child.appendChild(document.createElement("p"));
+        author.textContent = ` Author: ${book.author}`;
+        const yearPublished = child.appendChild(document.createElement("p"));
+        yearPublished.textContent = `Year Published: ${book.yearPublished}`;
+
+        const removeButton = child.appendChild(document.createElement("button"));
+        removeButton.textContent = 'x';
+        removeButton.addEventListener("click", () => { 
+            child.remove();
+            controller.removeFromLibrary(child.id);
+        });
+
+        const toggleButton = child.appendChild(document.createElement("button"));
+        book.read === true ? toggleButton.textContent = "Read" : toggleButton.textContent = "Haven't Read";
+        toggleButton.addEventListener("click", () => {
+            book.readToggle();
+            book.read === true ? toggleButton.textContent = "Read" : toggleButton.textContent = "Haven't Read";
+        });
+    };
+
+    return { renderNewBook };
+
+})();
+
+const controller = (() => {
+    const myLibrary = new Library();
+    
+    const addBookToLibrary = (title, author, yearPublished, bookRead) => {
+        const newBook = new Book(title, author, yearPublished, bookRead);
+        myLibrary.library.push(newBook);
+        eventListeners.renderNewBook(newBook);
+    }
+
+    const removeFromLibrary = (id) => {
+        const index = myLibrary.library.findIndex(book => book.id === id);
+        if (index !== -1){
+            myLibrary.library.splice(index, 1);
+        }
+    }
+
+    const showLibrary = () => { console.log(myLibrary) };
+
+    return { addBookToLibrary, showLibrary, removeFromLibrary };
+
+})();
